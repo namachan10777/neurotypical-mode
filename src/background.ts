7ca658f0d6b7b192eda511671e164cf16d66f345
Nakano Masaki <namachan10777@gmail.com>
2020-12-16 21:39:17 +0900

    close twitter

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,18 @@
 chrome.runtime.onInstalled.addListener(function() {
-  console.log("Hello World!");
+  let twitterRe = /https:\/\/(tweetdeck\.)?twitter\.com.*/;
+  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
+    if (changeInfo.url) {
+      console.log(changeInfo.url);
+      if (twitterRe.test(changeInfo.url)) {
+        console.log("close");
+        chrome.tabs.remove(tabId);
+      }
+    }
+  });
+  chrome.tabs.onCreated.addListener(function(tab) {
+    console.log(tab.url);
+    if (tab.id && tab.url && twitterRe.test(tab.url)) {
+      chrome.tabs.remove(tab.id);
+    }
+  });
 });
